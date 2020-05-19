a8c6bcc8a37b5fbad1ef782f510d9de233c838bf
Jessica <duellje@oregonstate.edu>
2020-05-19 11:53:00 -0400

    completed task 5

--- index.js
+++ index.js
@@ -74,15 +74,26 @@ function getYears(callBackFunction) {
 
 // console.log(getYears(getFinals));
 
-/* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
+/* Task 5: Implement a higher-order function called `getWinners`, that accepts the 
+callback function `getFinals()` and determine the winner (home or away) of each `finals` game.
+ Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
+function getWinners(callBackFunction) {
 
-    /* code here */
+    const finals = callBackFunction(fifaData);
+    const winners = [];
+    for(let i = 0; i < finals.length; i++){
+        if(finals[i]["Home Team Goals"] > finals[i]["Away Team Goals"]){
+           winners.push(finals[i]["Home Team Name"]);
+        } else {
+            winners.push(finals[i]["Away Team Name"]);
+        }
+    }
+    return winners;
 
 };
 
-getWinners();
+// console.log(getWinners(getFinals));
 
 /* Task 6: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
